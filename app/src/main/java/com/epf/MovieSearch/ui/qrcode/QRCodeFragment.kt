27ac6845416c2ea250c7ba3fcb2717e8e4118c9e
nicolasrapp <123481160+nicolasrapp@users.noreply.github.com>
2023-06-06 16:40:05 +0200

    added qr code

--- app/src/main/java/com/epf/MovieSearch/ui/qrcode/QRCodeFragment.kt
+++ app/src/main/java/com/epf/MovieSearch/ui/qrcode/QRCodeFragment.kt
@@ -1,60 +1,153 @@
 package com.epf.MovieSearch.ui.qrcode
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.epf.MovieSearch.R
+import androidx.lifecycle.lifecycleScope
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import com.epf.MovieSearch.databinding.FragmentQRCodeBinding
+import com.epf.MovieSearch.databinding.PreviewViewBinding
+import java.util.concurrent.Executors
+import androidx.core.content.ContextCompat
+import androidx.camera.core.Preview
+import androidx.camera.core.*
+import android.util.Size
+import java.util.concurrent.ExecutorService
+import android.widget.Toast
+import android.util.Log
+import android.content.Intent
+import com.epf.MovieSearch.ui.MovieDetailFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QRCodeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val CAMERA_PERMISSION_REQUEST_CODE = 1
+
 class QRCodeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: PreviewViewBinding
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var barcodeScanner: BarcodeScanner
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = PreviewViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (hasCameraPermission()) {
+            bindCameraUseCases()
+        } else {
+            requestPermission()
         }
     }
 
-    override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_r_code, container, false)
+    private fun hasCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            bindCameraUseCases()
+        } else {
+            Toast.makeText(requireContext(), "Camera permission required", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun bindCameraUseCases() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+
+        cameraProviderFuture.addListener({
+
+            cameraProvider = cameraProviderFuture.get()
+
+            val previewUseCase = Preview.Builder().build().apply {
+                setSurfaceProvider(binding.cameraView.surfaceProvider)
+            }
+
+            val options = BarcodeScannerOptions.Builder().setBarcodeFormats(
+                Barcode.FORMAT_CODE_128,
+                Barcode.FORMAT_CODE_39,
+                Barcode.FORMAT_CODE_93,
+                Barcode.FORMAT_EAN_8,
+                Barcode.FORMAT_EAN_13,
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_UPC_A,
+                Barcode.FORMAT_UPC_E,
+                Barcode.FORMAT_PDF417
+            ).build()
+
+            barcodeScanner = BarcodeScanning.getClient(options)
+
+            val analysisUseCase = ImageAnalysis.Builder().build().apply {
+                setAnalyzer(Executors.newSingleThreadExecutor(), ::processImageProxy)
+            }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, previewUseCase, analysisUseCase)
+            } catch (e: IllegalStateException) {
+                Log.e(TAG, e.message.orEmpty())
+            } catch (e: IllegalArgumentException) {
+                Log.e(TAG, e.message.orEmpty())
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun processImageProxy(imageProxy: ImageProxy) {
+        imageProxy.image?.let { image ->
+            val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+
+            barcodeScanner.process(inputImage)
+                .addOnSuccessListener { barcodeList ->
+                    val barcode = barcodeList.getOrNull(0)
+                    barcode?.rawValue?.let { value ->
+                        Log.d(TAG, "Scanned QR code value: $value") // Log the value
+
+                        val movieDetailFragment = MovieDetailFragment()
+                        val bundle = Bundle()
+                        bundle.putString("film_id", value)
+                        movieDetailFragment.arguments = bundle
+                        val intValue = value.toInt()
+
+                        // Launch the MovieDetailFragment within the current fragment's context
+                        val fragment = MovieDetailFragment.newInstance(intValue)
+
+                        val fragmentManager = requireActivity().supportFragmentManager
+
+                        // Remplacez le fragment actuel par le fragment MovieDetailFragment
+                        fragmentManager.beginTransaction()
+                            .replace(R.id.movieName, fragment)
+                            .addToBackStack(null) // Ajoutez la transaction à la pile de retour arrière
+                            .commit()
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e(TAG, it.message.orEmpty())
+                }
+                .addOnCompleteListener {
+                    imageProxy.image?.close()
+                    imageProxy.close()
+                }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QRCodeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QRCodeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "EPF"
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
     }
 }
